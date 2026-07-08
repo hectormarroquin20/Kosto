14278225c72ba96b4f24680b5a6cf1d3b459656c
hectormarroquin20 <oswaldo.margueta@gmail.com>
2026-07-08 17:08:37 -0600

    feat(i18n): add multilingual support with Spanish and English translations
    - Created `ui.ts` for language definitions and translations.
    - Implemented utility functions in `utils.ts` for language handling and translation retrieval.
    - Updated `Layout.astro` to dynamically set language based on URL and utilize translations.
    - Created localized pages for About, Contact, Index, and Privacy, replacing previous static pages.
    - Removed old static pages for About, Index, and Privacy.
    - Added TypeScript configuration for improved type checking and path resolution.

--- insummi-landing/astro.config.mjs
+++ insummi-landing/astro.config.mjs
@@ -8,6 +8,13 @@ export default defineConfig({
     format: 'directory', // Esto convierte about.html en about/index.html
     //format: 'file',
   },
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      strategy: 'prefix-except-default', // Esto hace que /es sea opcional y /en sea obligatorio o viceversa
+    },
+  },
 
   // site: 'https://insummi.com' // Replace with your actual domain
   // site: 'http://localhost:4321',
