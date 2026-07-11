9ec9731c1be745bf8bd4185d5420c2d3a0dce39f
hectormarroquin20 <oswaldo.margueta@gmail.com>
2026-07-11 17:45:44 -0600

    fix: update environment configuration for production deployment

--- kosto-frontend/src/environments/environment.ts
+++ kosto-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000',
+    production: true,
+    apiUrl: 'https://34fjhb5sh5.execute-api.us-east-1.amazonaws.com/Prod',
+    // apiUrl: 'http://localhost:3000',
     authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_eAzPdrLjW',
     redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
     postLogoutRedirectUri: 'https://d84l1y8p4kdic.cloudfront.net',
