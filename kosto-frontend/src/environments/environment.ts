2951854a3e9ffbfc56a47d16257e80dcb262d7cc
hectormarroquin20 <oswaldo.margueta@gmail.com>
2026-07-11 20:34:51 -0600

    fix: update redirectUrl to use the correct CloudFront distribution

--- kosto-frontend/src/environments/environment.ts
+++ kosto-frontend/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
     apiUrl: 'https://34fjhb5sh5.execute-api.us-east-1.amazonaws.com/Prod',
     // apiUrl: 'http://localhost:3000',
     authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_eAzPdrLjW',
-    redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
+    // redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
+    redirectUrl: 'd8jpbx679xp2v.cloudfront.net',
     postLogoutRedirectUri: 'https://d84l1y8p4kdic.cloudfront.net',
 };
