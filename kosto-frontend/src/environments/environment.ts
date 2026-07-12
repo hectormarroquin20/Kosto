60bf248ddca058b8a20a9015075ae6bb21252eed
hectormarroquin20 <oswaldo.margueta@gmail.com>
2026-07-11 20:52:11 -0600

    fix: update redirectUrl to use dynamic origin for callback

--- kosto-frontend/src/environments/environment.ts
+++ kosto-frontend/src/environments/environment.ts
@@ -2,8 +2,9 @@ export const environment = {
     production: true,
     apiUrl: 'https://34fjhb5sh5.execute-api.us-east-1.amazonaws.com/Prod',
     authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_eAzPdrLjW',
-    redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
+    // redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
     postLogoutRedirectUri: 'https://d84l1y8p4kdic.cloudfront.net',
     // redirectUrl: 'd8jpbx679xp2v.cloudfront.net',
     // postLogoutRedirectUri: 'https://d8jpbx679xp2v.cloudfront.net',
+    redirectUrl: window.location.origin + '/callback',
 };
